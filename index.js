cfea36f3f4f3ab4fcb2fb2674faab2472db6cc3b
Jarquevious <jarquevious@gmail.com>
2020-07-17 17:58:39 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Initialize express
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.get('/', (req, res) => {
+  res.render('home', { msg: 'Handlebars are Cool!' });
+})
+
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+  console.log('App listening on port 3000!')
+})
